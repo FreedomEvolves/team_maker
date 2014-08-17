cd481ffc77aff89e75800c7a7812a59524ba6d25
samskinner <samskinner@onesourcebrokers.com>
2014-08-17 18:04:39 -0400

    got ruby code integrated with project

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,42 @@
 class WelcomeController < ApplicationController
-  include WelcomeHelper
+#include WelcomeHelper
 
   def index
   end
 
   def teamlists
+   	name = params[:name]
+   	teams = params[:teams]
+  	@players = Student.all
+  	@teamnames = TeamName.all
+
+ # 	def roster_maker
+    rand_players = []
+    rand_players = @players.shuffle
+
+    stop = @players.length/@teamnames.length
+    add = (@players.length - (stop * @teamnames.length))
+
+    teamlist = []
+    (1..stop).each do
+       @teamnames.each do |name|
+          teamlist.push(name)
+       end
+    end
+
+    i = 0
+    while i < add
+       teamlist.push(@teamnames[i])
+       i = i + 1
+    end
+
+    sorted_list = []
+    sorted_list = teamlist.sort
+
+    @roster = {}
+    rand_players.each_with_index {|k,i|@roster[k] = sorted_list[i]}
+return @roster
   end
 end
+
+ 
