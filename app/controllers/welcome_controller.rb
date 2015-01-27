277e2f8979c6bf9bec62140a7522a5f04923d7a1
freedomevolves <samskinnerphd@gmail.com>
2015-01-27 09:00:59 -0500

    finish moving code to helper files

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,6 +8,7 @@ include WelcomeHelper
   end
 
   def teamlists
+    
    	name = params[:name]
    	teams = params[:teams]
   	@students = Student.all
@@ -18,38 +19,12 @@ include WelcomeHelper
 
     @teamnames = []
     team_maker @teamxs, @teamnames
+
+    roster_maker @players, @teamnames
  
+  end 
+end 
 
- # 	def roster_maker
-    rand_players = []
-    rand_players = @players.shuffle
-
-    stop = @players.length/@teamnames.length
-    add = (@players.length - (stop * @teamnames.length))
-
-    teamlist = []
-    (1..stop).each do
-       @teamnames.each do |name|
-          teamlist.push(name)
-       end
-    end
-
-    i = 0
-    while i < add
-       teamlist.push(@teamnames[i])
-       i = i + 1
-    end
-
-    sorted_list = []
-    sorted_list = teamlist.sort
-
-    @roster = {}
-    rand_players.each_with_index {|k,i|@roster[k] = sorted_list[i]}
-    puts @roster
-    return @roster
-  end
-
-end
 
  
 
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,45 +1,44 @@
 module WelcomeHelper
   
-def list_maker x, y
-  x.each do |student|
-  y.push(student.name)
-  end 
-end
+  def list_maker x, y
+    x.each do |student|
+      y.push(student.name)
+    end 
+  end
 
   def team_maker x, y
-  x.each do |student|
-  y.push(student.teams)
-  end 
-end  
+    x.each do |student|
+      y.push(student.teams)
+    end 
+  end  
 
-  def roster_mker
+  def roster_maker (players, teamnames)
     rand_players = []
     rand_players = @players.shuffle
 
-    stop = @players.length/teamnames.length
-    add = (players.length - (stop * teamnames.length))
+    stop = @players.length/@teamnames.length
+    add = (@players.length - (stop * @teamnames.length))
 
     teamlist = []
     (1..stop).each do
-       teamnames.each do |name|
-          teamlist.push(name)
-       end
+      @teamnames.each do |name|
+        teamlist.push(name)
+      end
     end
 
     i = 0
     while i < add
-       teamlist.push(teamnames[i])
-       i = i + 1
+      teamlist.push(@teamnames[i])
+      i = i + 1
     end
 
     sorted_list = []
     sorted_list = teamlist.sort
 
-    roster1 = {}
-    rand_players.each_with_index {|k,i|roster1[k] = sorted_list[i]}
-    return roster1
-  end
-
+    @roster = {}
+    rand_players.each_with_index {|k,i|@roster[k] = sorted_list[i]}
+    return @roster
+   end
 end
 
 
